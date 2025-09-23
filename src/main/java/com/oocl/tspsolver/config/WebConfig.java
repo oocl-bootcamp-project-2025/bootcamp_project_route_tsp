52265d9c74bf615cde419eb95833a682687b4da2
Andy Yip <andy.yip@oocl.com>
2025-09-23 11:43:06 +0800

    Fix: addCorsMappings

--- src/main/java/com/oocl/tspsolver/config/WebConfig.java
+++ src/main/java/com/oocl/tspsolver/config/WebConfig.java
@@ -2,11 +2,12 @@ package com.oocl.tspsolver.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMapping(CorsRegistry registry) {
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("GET", "POST")
                 .allowedHeaders("*");
